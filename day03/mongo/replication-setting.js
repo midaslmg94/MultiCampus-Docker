c8915cbaeac35e92f54592b7e0d686b5fe2e31cb
mk <midaslmg94@gmail.com>
2020-01-02 16:14:15 +0900

    Docker 191230 ~ 200102

---
+++ day03/mongo/replication-setting.js
@@ -0,0 +1,20 @@
+// replication setting #1
+/*rs.initiate();
+rs.add("172.17.0.2:27017");
+rs.add("172.17.0.3:27017");
+rs.add("172.17.0.4:27017");
+*/
+
+
+// replication setting #2
+
+config={
+    _id: "myapp",
+    members: [
+        {_id:0, host: "mongo1:27017"},
+        {_id:1, host: "mongo2:27017"},
+        {_id:2, host: "mongo3:27017"},
+    ]
+};
+rs.initiate(config);
+rs.config();
